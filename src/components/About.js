c9c5c15727c421677651bc90cb214b88b2b9ef36
Vishakha Singhal01 <singhalvishakha.career@gmail.com>
2024-01-04 13:09:08 +0530

    added about & contact page

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,42 @@
-import image from "../assets/aboutUs.svg";
+import React from 'react';
 
-const About = () => {
-  return (
-    <div className="h-full w-auto flex flex-wrap justify-center my-10 mx-auto">
-      <div>
-        <img className="h-[74.6vh]" src="{image}" alt="About Us" />
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+      count2: 0,
+    };
+
+    // console.log('constructor');
+  }
+
+  componentDidMount() {
+    // console.log('mounted');
+  }
+
+  render() {
+    // console.log('rendered');
+    return (
+      <div className='container-max py-16  text-center min-h-[80vh]'>
+        <img
+          src='https://akm-img-a-in.tosshub.com/indiatoday/images/story/201305/cookie-3_660_050913083828.jpg?VersionId=zkbUqSVkwYAkZ0zT4EOs.r5aLvOUicL7&size=690:388'
+          alt=''
+          className='w-full max-w-[480px] mx-auto rounded-lg'
+        />
+
+        <div className='w-[90%] max-w-[480px] mx-auto'>
+          <h1 className='text-3xl my-4'>Taste of India</h1>
+
+          <p>
+            A food ordering web application built with React.js ⚛ and
+            Swiggy's API.
+          </p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default About;
--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,13 +1,56 @@
-import image from "../assets/ContactUs.svg";
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline';
 
-const ContactUs = () => {
+const details = {
+  name: 'Vishakha Singhal',
+  bio: 'Frontend Developer',
+  contact: {
+    email: 'singhalvishakha.career@gmail.com',
+    github: 'https://github.com/vishakha-singhal01',
+    linkedin: 'https://www.linkedin.com/in/vishakha-singhal-18983b1bb',
+  },
+};
+
+const Contact = () => {
   return (
-    <div className="h-full w-auto flex flex-wrap justify-center my-10 mx-auto">
+    <div className='container-max min-h-screen ml-12'>
+      <img className='text-2xl my-4 font-bold' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7iXbo7SFGkBtFZNQ0xCMFSOfqeivJhZHrbw&usqp=CAU'/>
       <div>
-        <img className="h-[74.6vh]" src={image} alt="Contact Us" />
+        <h2 className='text-xl font-semibold'>Hi 👋, I'm {details.name} 👩‍🍳</h2>
+        <p className='text-lg'>{details.bio}</p>
+
+        <div className='my-4 space-y-2 ml-1'>
+          <h3 className='text-lg font-semibold'>Connect with me</h3>
+          <p className='flex flex-wrap items-center gap-2'>
+            <span className='font-semibold'>Gmail: </span>
+            <a href={details.contact.email} className='flex items-center gap-1'>
+              {details.contact.email}
+              <ArrowTopRightOnSquareIcon className='w-4 h-4' />
+            </a>
+          </p>
+          <p className='flex flex-wrap items-center gap-2'>
+            <span className='font-semibold'>Github: </span>{' '}
+            <a
+              href={details.contact.github}
+              className='flex items-center gap-1'
+            >
+              {details.contact.github}
+              <ArrowTopRightOnSquareIcon className='w-4 h-4' />
+            </a>
+          </p>
+          <p className='flex flex-wrap items-center gap-2'>
+            <span className='font-semibold'>Linkedin: </span>{' '}
+            <a
+              href={details.contact.linkedin}
+              className='flex items-center gap-1'
+            >
+              {details.contact.linkedin}
+              <ArrowTopRightOnSquareIcon className='w-4 h-4' />
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
 };
 
-export default ContactUs;
+export default Contact;
